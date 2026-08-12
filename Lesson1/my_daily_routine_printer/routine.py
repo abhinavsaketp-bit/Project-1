30062a5970f2b849103e27f7fb80fff779698bef
abhinavsaketp <abhinavsaket.p@gmail.com>
2026-08-12 19:24:41 +0100

    my_daily_routine_printer

---
+++ Lesson1/my_daily_routine_printer/routine.py
@@ -0,0 +1,13 @@
+print("My Daily Routine")
+#waking up
+print("I wake up at 6:30 in the morning ")
+#school
+print("I go to school at 8:20am ")
+#meals
+print("I have my lunch at 12:00pm in school")
+#studing 
+print("I study for 2 hours in the evening")
+#sleep
+print("I go to bed by 9:30pm")
+#study time per week
+print("Total study hours in a week:" , 2*7)
