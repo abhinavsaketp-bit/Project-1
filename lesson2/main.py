f787c82096a5d948428ec29ad138615158a4bdba
abhinavsaketp <abhinavsaket.p@gmail.com>
2026-08-13 20:38:55 +0100

    Main project

---
+++ lesson2/main.py
@@ -0,0 +1,8 @@
+print("hello to the world of coding")
+print(4)
+print(5)
+print("4+5")
+print(4+5)
+age=13
+print("my age is ",age)
+print(f"my age is {age}")
---
+++ lesson2/variable.py
@@ -0,0 +1,7 @@
+x=4
+y=8
+print(f"the value of y is{y} and the value of x is{x}")
+name=input(" what is your name?")
+print(name)
+print(" the entered name is ",name)
+print("the addition of x and y is ", x+y)
