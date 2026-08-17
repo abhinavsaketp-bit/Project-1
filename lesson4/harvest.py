29e5bd2459e7be3b12a8195df69db4bbeead50c5
abhinavsaketp <abhinavsaket.p@gmail.com>
2026-08-17 11:44:34 +0100

    harvest.py

---
+++ lesson4/harvest.py
@@ -0,0 +1,30 @@
+t1=int(input("enter number 1:"))
+t2=int(input("enter number 2:"))
+t3=int(input("enter number 3:"))
+t4=int(input("enter number 4:"))
+t5=int(input("enter number 5:"))
+total= t1+t2+t3+t4+t5
+average= total/5
+print("Total harvest is", total)
+print("Total average is", average)
+bags= total//25
+left_over= total%25
+print("Number of bags i can make is", bags)
+print("Number of leftover is", left_over)
+x=10
+y=2
+print(x==y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+print(x!=y)
+
+Abhinav=25
+print(Abhinav)
+Abhinav+=2
+print(Abhinav)
+Abhinav*=3
+print(Abhinav)
+Abhinav/=9
+print(Abhinav)
