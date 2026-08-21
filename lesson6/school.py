dc1289298cb551a2e16d5ab68948b9a8cf65575f
abhinavsaketp <abhinavsaket.p@gmail.com>
2026-08-21 11:05:41 +0100

    planner

--- lesson6/school.py
+++ lesson6/school.py
@@ -1,13 +1,25 @@
 print("I am a smart student planner ")
 print("I will ask you 3 questions ")
 day=input("what day is it today? ").lower()
-weather=input("How is the weather today? (Sunny or rainy)")
+weather=input("How is the weather today? (sunny or rainy)").strip().lower()
 homework=input("have you done your homwork(yes or no)")
 if day== "saturday" or  day=="sunday":
     print("It is the right time to go")
 elif day== "monday" or day=="tusday":
     print("It is school time")
-elif day== "wednesday" or day=="thursday" or day=="friday":
+elif day in ("wednesday","thursday","friday"):
     print("It is still school day")
 else:
     print("enter a valid day")
+if weather=="sunny" and homework=="yes":
+    print("Enjoy your trip outside.")
+if not homework=="yes":
+    print("Complete your homework first")
+if weather=="sunny" and day in ("saturday","sunday"):
+    print("Plan for a trip outside")
+elif weather=="sunny" and homework=="yes" and day in ("saturday","sunday"):
+    print("It is time to play with your friends")
+elif weather=="rainy" and homework=="yes":
+    print(" Have indoor free time.")
+else:
+    print("Best plan is to enjoy with your family.")
