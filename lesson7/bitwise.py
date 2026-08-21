3ee4c8ed5c083830e240822d34809994cda1f354
abhinavsaketp <abhinavsaket.p@gmail.com>
2026-08-21 11:09:15 +0100

    identity

---
+++ lesson7/bitwise.py
@@ -0,0 +1,9 @@
+a=7
+b=5
+print(bin(7))
+print(bin(5))
+print(a & b)
+print(a|b)
+print(~a)
+print(a>>2)
+print(a<<2)
---
+++ lesson7/identity.py
@@ -0,0 +1,9 @@
+a=[1,2,3]
+b=[1,2,3]
+c=a
+print(a is b)
+print(a==b)
+print(a is not b)
+print(a==c)
+print(a is c)
+
