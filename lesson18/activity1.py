34bab6a61d0640cf2011e95d41543c7b0d0f43d7
abhinavsaketp <abhinavsaket.p@gmail.com>
2026-09-25 07:46:00 +0100

    Try block

---
+++ lesson18/activity1.py
@@ -0,0 +1,6 @@
+try:
+    value=int(input("Enter a value: "))
+    print(value)
+except ValueError:
+    print("please only enter a intiger value")
+    
---
+++ lesson18/activity2.py
@@ -0,0 +1,15 @@
+try:
+    print("Enter 2 numbers seprated by a comma")
+    num1,num2=eval(input("Enter the two number"))
+    print("The division of the two number is", num1/num2)
+except ZeroDivisionError:
+    print("The denominator is 0 so it is a 0 dision error")
+except SyntaxError as e:
+    print("This is a syntax error.", e)
+except:
+    print("There is an exception in the input.")
+
+finally:
+    print("This block is printing no matter what")
+
+
