a32c51860429a1493b085b8b9ffe9fdbf4bc4c88
abhinavsaketp <abhinavsaket.p@gmail.com>
2026-09-26 19:17:47 +0100

    SUM

---
+++ lesson17/HW1.py
@@ -0,0 +1,16 @@
+
+N=int(input("Enter a number to find the sum of 1-entered number and find the average of it: "))
+sum=0
+
+for i in range(1,N+1):
+    sum+=i
+print("This is the sum of the number given:", sum)
+
+average= sum/N
+
+print("The average is:", average)
+
+    
+
+
+    
